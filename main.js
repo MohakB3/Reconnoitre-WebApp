a3cd1341a77ad4a59b1db53e4966e06dd6e8a0ff
MohakB3 <Iammohakb@gmail.com>
2023-01-27 11:07:55 -0800

    Commit 1 27/01/2023
    Added client-side JSONP Validation Functionality

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 function validateInput() {
-    console.log("Hello World!");
+    $.ajax({
+        url: "https://52.15.145.92:4906/",
+        dataType: "jsonp", // jsonp
+        type: "POST",
+        jsonpCallback: 'processJSONPResponse', // add this property
+        contentType: "application/json; charset=utf-8",
+        success: function (result, status, xhr) {
+                let userInput = document.getElementById("code_input").value;
+                if(userInput == result.code) {
+                    console.log("Input Validated");
+                    alert(result.message);
+                }
+                else {
+                    console.log("Error");
+                    alert(result.code);
+                }
+                // console.log(result.message);
+                // console.log(result.code);
+        },
+        error: function (xhr, status, error) {
+            console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        }
+    });
 }
-
-$.getJSON('http://localhost:4906/data', function(data) {
-    // JSON result in `data` variable
-});
